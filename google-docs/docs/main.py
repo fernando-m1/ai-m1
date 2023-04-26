eab78fdb35d8d8503b39ecf0cf1243464502febe
fernando-m1 <130691741+fernando-m1@users.noreply.github.com>
2023-04-25 19:02:46 -0600

    Se corrigió error mínimo

--- google-docs/docs/main.py
+++ google-docs/docs/main.py
@@ -134,21 +134,3 @@ st.markdown("#### El documento contiene el siguiente texto:")
 if doc_id:
   doc_text = read_structural_elements(doc_content)
   st.write(doc_text)
-
-
-
-
-
-def write_creds():
-    """ Write service account credentials to disk based on environment variables."""
-    credentials = {
-        "type": "service_account",
-        "project_id": os.getenv("GOOGLE_PROJECT_ID"),
-        "private_key_id": os.getenv("GOOGLE_PRIVATE_KEY_ID"),
-        "private_key": os.getenv("GOOGLE_PRIVATE_KEY"),
-        "client_email": os.getenv("GOOGLE_CLIENT_EMAIL"),
-        "client_id": os.getenv("GOOGLE_CLIENT_EMAIL"),
-        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-        "token_uri": "https://oauth2.googleapis.com/token",
-        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-        "client_x509_cert_url": os.getenv("GOOGLE_CLIENT_X509_CERT_URL")
