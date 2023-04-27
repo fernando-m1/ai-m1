db829c2324e4bae48d1b6fa17be7fd3c95e67787
fernando-m1 <130691741+fernando-m1@users.noreply.github.com>
2023-04-26 19:05:36 -0600

    cambios

--- google-docs/docs/main.py
+++ google-docs/docs/main.py
@@ -46,13 +46,18 @@ from apiclient.discovery import build
 from oauth2client.service_account import ServiceAccountCredentials
 
 import requests
-r = requests.get('https://github.com/fernando-m1/ai-m1/blob/8057dac908b57c71bb320be3b3f2875282a283d6/google-docs/docs/credentials.json')
+response = requests.get('https://raw.githubusercontent.com/fernando-m1/ai-m1/main/google-docs/docs/credentials.json')
 
 
 SCOPES = 'https://www.googleapis.com/auth/documents.readonly'
 DISCOVERY_DOC = 'https://docs.googleapis.com/$discovery/rest?version=v1'
 DOCUMENT_ID = '1SwVlU6ZKArnW9pfEQCi5YmaMPr2TkSrseRd-0PQ5Ys0'
-SERVICE_ACCOUNT_FILE = r.json()
+SERVICE_ACCOUNT_FILE = response.json()
+
+json_file_path = "/path/to/example.json"
+
+with open(json_file_path, 'r') as j:
+     contents = json.loads(j.read())
 
 
 def get_credentials():
