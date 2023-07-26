a4108c582e11a963ad67aadc1dfe12d22e68de02
fernando-m1 <130691741+fernando-m1@users.noreply.github.com>
2023-07-25 19:15:08 -0600

    Update main.py

--- justino/main.py
+++ justino/main.py
@@ -7,6 +7,81 @@ from langchain.llms import OpenAI
 
 OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
 
+# Set Google Docs API environment variables
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/documents.readonly']
+DISCOVERY_DOC = 'https://docs.googleapis.com/$discovery/rest?version=v1'
+
+# The Document ID of the contract agreement to work with.
+DOCUMENT_ID = '1SwVlU6ZKArnW9pfEQCi5YmaMPr2TkSrseRd-0PQ5Ys0'    # NECESITAMOS CAMBIAR AQUÍ PARA QUE LA BASE DE DATOS ALIMENTE ESTE VALOR
+
+### GOOGLE DOCS TEXT ###
+
+# Define credentials to access the Google Docs API
+def get_credentials():
+    
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+    return creds
+
+def read_paragraph_element(element):
+    """Returns the text in the given ParagraphElement.
+
+        Args:
+            element: a ParagraphElement from a Google Doc.
+    """
+    text_run = element.get('textRun')
+    if not text_run:
+        return ''
+    return text_run.get('content')
+
+def read_structural_elements(elements):
+    """Recurses through a list of Structural Elements to read a document's text where text may be
+        in nested elements.
+
+        Args:
+            elements: a list of Structural Elements.
+    """
+    text = ''
+    for value in elements:
+        if 'paragraph' in value:
+            elements = value.get('paragraph').get('elements')
+            for elem in elements:
+                text += read_paragraph_element(elem)
+        elif 'table' in value:
+            # The text in table cells are in nested Structural Elements and tables may be
+            # nested.
+            table = value.get('table')
+            for row in table.get('tableRows'):
+                cells = row.get('tableCells')
+                for cell in cells:
+                    text += read_structural_elements(cell.get('content'))
+        elif 'tableOfContents' in value:
+            # The text in the TOC is also in a Structural Element.
+            toc = value.get('tableOfContents')
+            text += read_structural_elements(toc.get('content'))
+    return text
+
+doc_content = read_gdocs_style()
+
+text = read_structural_elements(doc_content)
+
 def load_chain():
     """Logic for loading the chain you want to use should go here."""
     llm = OpenAI(temperature=0)
@@ -30,6 +105,7 @@ def get_text():
     input_text = st.text_input("You: ", "Hello, how are you?", key="input")
     return input_text
 
+st.text(text)
 
 user_input = get_text()
 
