3a950ccec5e22c5c5ba540e578c6a9d5eb65923b
fernando-m1 <130691741+fernando-m1@users.noreply.github.com>
2023-09-13 15:13:00 -0700

    Update main.py

--- grocerybot-demo/main.py
+++ grocerybot-demo/main.py
@@ -38,7 +38,7 @@ from langchain.prompts import MessagesPlaceholder
 
 
 # __import__('pysqlite3')
-import pysqlite3
+# import pysqlite3
 import sys
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
