73f4ff843d9b62abe4b056df778ebcc8f083b203
fernando-m1 <130691741+fernando-m1@users.noreply.github.com>
2023-09-13 15:10:53 -0700

    Update main.py

--- grocerybot-demo/main.py
+++ grocerybot-demo/main.py
@@ -40,6 +40,7 @@ from langchain.prompts import MessagesPlaceholder
 __import__('pysqlite3')
 import sys
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+import pysqlite3
 
 st.set_page_config(
     page_title="Morada Uno",
