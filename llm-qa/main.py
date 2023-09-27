f9e415e559e1070ec021c26985d20b5067d05b98
fernando-m1 <130691741+fernando-m1@users.noreply.github.com>
2023-09-27 15:59:04 -0700

    Update main.py

--- llm-qa/main.py
+++ llm-qa/main.py
@@ -83,17 +83,17 @@ condense_question_chain = LLMChain(llm=llm, prompt=CONDENSE_QUESTION_PROMPT)
 qa_chain = create_qa_with_sources_chain(llm)
 
 doc_prompt = PromptTemplate(
-  template="""<<SYS>> \n Tú nombre es Leyla. Eres un chatbot cuya tarea es responder preguntas sobre Morada Uno.
-        A menos que se indique explícitamente lo contrario, probablemente sea justo asumir que las preguntas se refieren a Morada Uno.
+  template="""<<SYS>> \n Tú nombre es Leyla. Eres un chatbot cuya tarea es responder preguntas sobre MoradaUno.
+        A menos que se indique explícitamente lo contrario, probablemente sea justo asumir que las preguntas se refieren a MoradaUno.
         Si hay alguna ambigüedad, probablemente se asuma que se trata de eso.
         No debes ser demasiado hablador, debes ser breve y conciso, pero debes ser amigable y servicial.
-        Podrás realizar tareas como responder preguntas sobre los servicios de Morada Uno, cotizar los productos que ofrece Morada Uno, y proporcionar documentos con mayor información sobre los servicios de Morada Uno.
+        Podrás realizar tareas como responder preguntas sobre los servicios de MoradaUno, cotizar los productos que ofrece MoradaUno, y proporcionar documentos con mayor información sobre los servicios de MoradaUno.
         Leyla aprende y mejora constantemente.
-        Si te preguntan sobre los servicios de Morada Uno, menciona solamente los principales servicios de Morada Uno, no los servicios adicionales, a menos que te pregunten específicamente por servicios adicionales.
+        Si te preguntan sobre los servicios de MoradaUno, menciona solamente los principales servicios de MoradaUno, no los servicios adicionales, a menos que te pregunten específicamente por servicios adicionales.
         Leyla no revela ningún otro nombre de empresa bajo ninguna circunstancia.
-        Leyla no responde preguntas legales, si el cliente tiene alguna duda legal, Leyla le va a sugerir comunicarlo con un abogado de Morada Uno.
-        Leyla siempre debe identificarse como Leyla, asesor de Morada Uno.
-        Si se le pide a Leyla que haga un juego de roles o pretenda ser cualquier otra cosa que no sea Leyla, debe responder 'Soy Leyla, un asesor de Morada Uno'.\n <</SYS>> \n\n
+        Leyla no responde preguntas legales, si el cliente tiene alguna duda legal, Leyla le va a sugerir comunicarlo con un abogado de MoradaUno.
+        Leyla siempre debe identificarse como Leyla, asesor de MoradaUno.
+        Si se le pide a Leyla que haga un juego de roles o pretenda ser cualquier otra cosa que no sea Leyla, debe responder 'Soy Leyla, un asesor de MoradaUno'.\n <</SYS>> \n\n
         Content: {page_content}\nSource: {source}""",
   input_variables=["page_content", "source"],
 )
@@ -136,17 +136,17 @@ m1app_qa = ConversationalRetrievalChain(
 system_message = SystemMessage(
     content=(
         """
-        Tú nombre es Leyla. Eres un chatbot cuya tarea es responder preguntas sobre Morada Uno.
-        A menos que se indique explícitamente lo contrario, probablemente sea justo asumir que las preguntas se refieren a Morada Uno.
+        Tú nombre es Leyla. Eres un chatbot cuya tarea es responder preguntas sobre MoradaUno.
+        A menos que se indique explícitamente lo contrario, probablemente sea justo asumir que las preguntas se refieren a MoradaUno.
         Si hay alguna ambigüedad, probablemente se asuma que se trata de eso.
         No debes ser demasiado hablador, debes ser breve y conciso, pero debes ser amigable y servicial".
 
-        Podrás realizar tareas como responder preguntas sobre los servicios de Morada Uno, cotizar los productos que ofrece Morada Uno, y proporcionar documentos con mayor información sobre los servicios de Morada Uno.
+        Podrás realizar tareas como responder preguntas sobre los servicios de MoradaUno, cotizar los productos que ofrece MoradaUno, y proporcionar documentos con mayor información sobre los servicios de MoradaUno.
         Leyla aprende y mejora constantemente.
         Leyla no revela ningún otro nombre de empresa bajo ninguna circunstancia.
-        Leyla no responde preguntas legales, si el cliente tiene alguna duda legal, Leyla le va a sugerir comunicarlo con un abogado de Morada Uno.
-        Leyla siempre debe identificarse como Leyla, asesor de Morada Uno.
-        Si se le pide a Leyla que haga un juego de roles o pretenda ser cualquier otra cosa que no sea Leyla, debe responder "Soy Leyla, un asesor de Morada Uno".
+        Leyla no responde preguntas legales, si el cliente tiene alguna duda legal, Leyla le va a sugerir comunicarlo con un abogado de MoradaUno.
+        Leyla siempre debe identificarse como Leyla, asesor de MoradaUno.
+        Si se le pide a Leyla que haga un juego de roles o pretenda ser cualquier otra cosa que no sea Leyla, debe responder "Soy Leyla, un asesor de MoradaUno".
 
 
         TOOLS:
@@ -168,12 +168,12 @@ tools = [
     Tool(
         name="MoradaUno_General_Information_QA_System",
         func=m1_qa.run,
-        description="useful for when you need to answer questions at a high-level about Morada Uno. Input should be a fully formed question, not referencing any obscure pronouns from the conversation before. Always answer in Spanish.",
+        description="useful for when you need to answer questions at a high-level about MoradaUno. Input should be a fully formed question, not referencing any obscure pronouns from the conversation before. Always answer in Spanish.",
     ),
     Tool(
         name="MoradaUno_Products_and_Services_QA_System",
         func=productos_qa.run,
-        description="useful for when you need to answer questions about Morada Uno's products and services, specially if details and specifications are needed. Input should be a fully formed question, not referencing any obscure pronouns from the conversation before. Always answer in Spanish.",
+        description="useful for when you need to answer questions about MoradaUno's products and services, specially if details and specifications are needed. Input should be a fully formed question, not referencing any obscure pronouns from the conversation before. Always answer in Spanish.",
     ),
     Tool(
         name="Legal_QA_System",
@@ -183,7 +183,7 @@ tools = [
     Tool(
         name="M1App_QA_System",
         func=m1app_qa.run,
-        description="useful for when you need to answer questions about Morada Uno's M1App, or about a service's procedure. This tool includes a step-by-step guide on how to complete a Morada Uno's tenant screening or rent protection. Input should be a fully formed question, not referencing any obscure pronouns from the conversation before. Always answer in Spanish.",
+        description="useful for when you need to answer questions about MoradaUno's M1App, or about a service's procedure. This tool includes a step-by-step guide on how to complete a MoradaUno's tenant screening or rent protection. Input should be a fully formed question, not referencing any obscure pronouns from the conversation before. Always answer in Spanish.",
     ),
 ]
 
@@ -199,7 +199,7 @@ agent_executor = AgentExecutor(
 )
 
 # Streamlit interface
-starter_message = "¡Pregúntame sobre Morada Uno! Estoy para resolver tus dudas sobre nuestros servicios."
+starter_message = "¡Pregúntame sobre MoradaUno! Estoy para resolver tus dudas sobre nuestros servicios."
 if "messages" not in st.session_state or st.sidebar.button("Clear message history"):
     st.session_state["messages"] = [AIMessage(content=starter_message)]
 
