c466f8bc6de1a48c0dbf30ed91ab7953127c8c0c
fernando-m1 <130691741+fernando-m1@users.noreply.github.com>
2023-09-29 13:08:24 -0700

    Update html_elements.py

--- llm-qa/html_elements.py
+++ llm-qa/html_elements.py
@@ -3,14 +3,14 @@ def get_navbar():
     <div data-animation="default" data-collapse="medium" data-duration="400" data-easing="ease" data-easing2="ease" role="banner" class="navbar-no-shadow-container-2 w-nav">
         <div class="container-regular-2">
             <div class="navbar-wrapper-3">
-                <a href="/" aria-current="page" class="navbar-brand-3 w-nav-brand w--current" aria-label="home">
+                <a href="https://www.moradauno.com.mx/" aria-current="page" class="navbar-brand-3 w-nav-brand w--current" aria-label="home">
                     <img width="240" alt="" src="https://global-uploads.webflow.com/5f4c3ae55a368d55645df6f8/646bb7659ada2ac56abf9585_Recurso%2025.svg" loading="lazy">
                 </a>
                 <nav role="navigation" class="nav-menu-wrapper-3 w-nav-menu">
                     <ul role="list" class="nav-menu-7 w-list-unstyled">
-                        <li><a href="/" aria-current="page" class="nav-link-18 w--current">Inmobiliarios</a></li>
-                        <li><a href="/propietarios" class="nav-link-18">Propietarios</a></li>
-                        <li><a href="/sobre-nosotros" class="nav-link-18">Sobre nosotros</a></li>
+                        <li><a href="https://www.moradauno.com.mx/" aria-current="page" class="nav-link-18 w--current">Inmobiliarios</a></li>
+                        <li><a href="https://www.moradauno.com.mx/propietarios" class="nav-link-18">Propietarios</a></li>
+                        <li><a href="https://www.moradauno.com.mx/sobre-nosotros" class="nav-link-18">Sobre nosotros</a></li>
                         <li class="mobile-margin-top-12">
                             <div class="nav-button-wrapper-2">
                                 <a href="https://asesor.moradaunoapp.com/#/app/introduccion" target="_blank" class="button-primary-5 w-button">Entrar a la M1App</a>
