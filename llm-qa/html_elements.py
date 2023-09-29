6718d36767c29c6a1bbf5874675a1db0930e7835
fernando-m1 <130691741+fernando-m1@users.noreply.github.com>
2023-09-29 13:32:47 -0700

    Update html_elements.py

--- llm-qa/html_elements.py
+++ llm-qa/html_elements.py
@@ -1,28 +1,25 @@
 def get_navbar():
     return """
-    <div data-animation="default" data-collapse="medium" data-duration="400" data-easing="ease" data-easing2="ease" role="banner" class="navbar-no-shadow-container-2 w-nav">
-        <div class="container-regular-2">
-            <div class="navbar-wrapper-3">
-                <a href="https://www.moradauno.com.mx/" aria-current="page" class="navbar-brand-3 w-nav-brand w--current" aria-label="home">
-                    <img width="240" alt="" src="https://global-uploads.webflow.com/5f4c3ae55a368d55645df6f8/646bb7659ada2ac56abf9585_Recurso%2025.svg" loading="lazy">
-                </a>
-                <nav role="navigation" class="nav-menu-wrapper-3 w-nav-menu">
-                    <ul role="list" class="nav-menu-7 w-list-unstyled">
-                        <li><a href="https://www.moradauno.com.mx/" aria-current="page" class="nav-link-18 w--current">Inmobiliarios</a></li>
-                        <li><a href="https://www.moradauno.com.mx/propietarios" class="nav-link-18">Propietarios</a></li>
-                        <li><a href="https://www.moradauno.com.mx/sobre-nosotros" class="nav-link-18">Sobre nosotros</a></li>
-                        <li class="mobile-margin-top-12">
-                            <div class="nav-button-wrapper-2">
-                                <a href="https://asesor.moradaunoapp.com/#/app/introduccion" target="_blank" class="button-primary-5 w-button">Entrar a la M1App</a>
-                            </div>
-                        </li>
-                    </ul>
-                </nav>
-                <div class="menu-button-6 w-nav-button" style="-webkit-user-select: text;" aria-label="menu" role="button" tabindex="0" aria-controls="w-nav-overlay-0" aria-haspopup="menu" aria-expanded="false">
-                    <div class="w-icon-nav-menu"></div>
-                </div>
-            </div>
-        </div>
-        <div class="w-nav-overlay" data-wf-ignore="" id="w-nav-overlay-0"></div>
-    </div>
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #3498DB;">
+      <a class="navbar-brand" href="/">MoradaUno</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link" href="https://www.moradauno.com.mx/">Inmobiliarios</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="https://www.moradauno.com.mx/propietarios">Propietarios</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="https://www.moradauno.com.mx/sobre-nosotros">Sobre nosotros</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="https://asesor.moradaunoapp.com/#/app/introduccion" target="_blank">Entrar a la M1App</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
     """
