b9ed2fb5121ab66db792a0b37974beab74652f3d
fernando-m1 <130691741+fernando-m1@users.noreply.github.com>
2023-09-29 12:24:46 -0700

    Create styles.py

---
+++ llm-qa/styles.py
@@ -0,0 +1,25 @@
+def get_styles():
+    return """
+        .navbar-wrapper-3 {
+            width: auto;
+            justify-content: space-between;
+            align-items: center;
+            display: flex;
+        }
+
+        body {
+            color: #333;
+            font-family: Arial, Helvetica Neue, Helvetica, sans-serif;
+            font-size: 14px;
+            line-height: 20px;
+            min-height: 100%;
+            background-color: #fff;
+            margin: 0;
+        }
+
+        html {
+            -ms-text-size-adjust: 100%;
+            -webkit-text-size-adjust: 100%;
+            font-family: sans-serif;
+        }
+    """
