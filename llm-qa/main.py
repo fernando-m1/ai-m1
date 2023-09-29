bee319c8cfbecea7748fcd655b337288f08a0de4
fernando-m1 <130691741+fernando-m1@users.noreply.github.com>
2023-09-29 15:17:23 -0700

    Update main.py

--- llm-qa/main.py
+++ llm-qa/main.py
@@ -23,89 +23,32 @@ from io import BytesIO
 # Streamlit page config
 st.set_page_config(page_title="MoradaUno Chatbot", page_icon="🤖")
 
-# HTML, CSS and JavaScript
-css_code = """
-    <style>
-        .navbar-wrapper-3 {
-            width: auto;
-            justify-content: space-between;
-            align-items: center;
-            display: flex;
-        }
-
-        body {
-            color: #333;
-            font-family: Arial, Helvetica Neue, Helvetica, sans-serif;
-            font-size: 14px;
-            line-height: 20px;
-            min-height: 100%;
-            background-color: #fff;
-            margin: 0;
-        }
-
-        html {
-            -ms-text-size-adjust: 100%;
-            -webkit-text-size-adjust: 100%;
-            font-family: sans-serif;
-        }
-
-        /* Add this to change the color of the navbar links */
-        .navbar-dark .navbar-nav .nav-link {
-            color: #671e75;
-        }
-
-        /* If you want to change the color on hover as well */
-        .navbar-dark .navbar-nav .nav-link {
-            color: #671e75;  /* This is the color you wanted */
-        }
-
-        /* Positioning of Navbar */
-        .navbar {
-            position: fixed !important;
-            top: 0;
-            width: 100%;
-            z-index: 1000;
-            height: 60px;  /* Adjust this value as needed */
-            padding: 10px 0;  /* This adds 10px padding to the top and bottom */
-}
-    </style>
-    """
-
-html_code = """
-    <nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #FAFAFA; height: 60px; padding: 10px 0;">
-      <a class="navbar-brand" href="/" style="color: #671e75;">MoradaUno</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item active">
-            <a class="nav-link" href="https://www.moradauno.com.mx/" style="color: #671e75;">Inmobiliarios</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="https://www.moradauno.com.mx/propietarios" style="color: #671e75;">Propietarios</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="https://www.moradauno.com.mx/sobre-nosotros" style="color: #671e75;">Sobre nosotros</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="https://asesor.moradaunoapp.com/#/app/introduccion" target="_blank" style="color: #671e75;">Entrar a la M1App</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-    """
-
-
-javascript_code = """
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    """
-
-# import and reference the external HTML and CSS style and JavaScript script files
 st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
-st.markdown(html_code, unsafe_allow_html=True)
+
+# HTML, CSS and JavaScript
+st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
+
+st.markdown("""
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #3498DB;">
+  <a class="navbar-brand" href="https://youtube.com/dataprofessor" target="_blank">Data Professor</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link disabled" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="https://youtube.com/dataprofessor" target="_blank">YouTube</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="https://twitter.com/thedataprof" target="_blank">Twitter</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+""", unsafe_allow_html=True)
 
 # Page title
 st.title("MoradaUno Chatbot 🤖")
@@ -318,4 +261,8 @@ if prompt := st.chat_input(placeholder=starter_message):
     st.chat_message("assistant", avatar=img).write(response_content)
 
 
-st.markdown(javascript_code, unsafe_allow_html=True)
+st.markdown("""
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+""", unsafe_allow_html=True
