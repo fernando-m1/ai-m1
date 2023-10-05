b19927b70598e67fb58a3e6546c92170239f0993
fernando-m1 <130691741+fernando-m1@users.noreply.github.com>
2023-10-05 15:05:28 -0700

    Update main.py

--- llm-qa/main.py
+++ llm-qa/main.py
@@ -234,10 +234,3 @@ if prompt := st.chat_input(placeholder=starter_message):
     
     st.session_state.messages.append(AIMessage(content=response_content))
     st.chat_message("assistant", avatar=img).write(response_content)
-
-
-st.markdown("""
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-""", unsafe_allow_html=True)
